0347096da1ef5588d9e26968071b35ee49a8f863
Meicheng-SEU <48236166+Meicheng-SEU@users.noreply.github.com>
2020-05-05 23:18:58 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,11 @@
 
 url=https://archive.physionet.org/users/shared/challenge-2019/
 
-mkdir data && cd data
-# how to build a new die
+mkdir data prediction label xgb_model
+
+cd data
+mkdir all_dataset
+
 curl -O $url/training_setA.zip
 unzip training_setA.zip
 curl -O $url/training_setB.zip
@@ -12,4 +15,3 @@ unzip training_setA.zip
 cd ..
 
 python build_datasets.py
-
