4da0dc9ffa1b2d2981c1ce4dd1414a3cfa32c71d
Meicheng-SEU <48236166+Meicheng-SEU@users.noreply.github.com>
2020-05-05 23:19:23 +0800

    Update shap_explain.py

--- shap_explain.py
+++ shap_explain.py
@@ -2,26 +2,31 @@ import xgboost as xgb
 import shap
 import numpy as np
 import pandas as pd
+from feature_engineering import data_process
 
-def load_model_shap(X_test, k_fold, path):  # 加载训练好的模型
+def shap_value(input_data, k_fold, model_path):
     shap.initjs()
-    expected_value = np.zeros((k_fold, 1))
-    test_pred = np.zeros((X_test.shape[0], k_fold))
-    all_shap_values = np.zeros((X_test.shape[0], X_test.shape[1]))
-    X_test = xgb.DMatrix(X_test)
+    all_shap_values = np.zeros((input_data.shape[0], input_data.shape[1]))
+    dat = xgb.DMatrix(input_data)
     for k in range(k_fold):
-        model_path = path + '/model{}.mdl'.format(k + 1)
-        xgb_model = xgb.Booster(model_file=model_path)
-        # y_test_pred = xgb_model.predict(X_test)
-        # test_pred[:, k] = y_test_pred
+        file_name = model_path + 'model{}.mdl'.format(k + 1)
+        xgb_model = xgb.Booster(model_file=file_name)
 
         explainer = shap.TreeExplainer(xgb_model)
-        expected_value[k] = explainer.expected_value
-        shap_values = explainer.shap_values(X_test)
+        shap_values = explainer.shap_values(dat)
         all_shap_values = all_shap_values + shap_values
 
-    # test_pred = pd.DataFrame(test_pred)
-    # result_pro = test_pred.mean(axis=1)
-    expected_value = np.mean(expected_value)
+    return all_shap_values / 5
 
-    return (all_shap_values / 5), expected_value
+if __name__ == "__main__":
+    data_path = "./data/all_dataset/"
+    train_nosepsis = np.load('./data/train_nosepsis.npy')
+    train_sepsis = np.load('./data/train_sepsis.npy')
+
+    train_set = np.append(train_sepsis, train_nosepsis)
+    features, labels = data_process(train_set, data_path)
+
+    shap_data = shap_value(features, k_fold=5, path='./xgb_model/')
+    shap.summary_plot(shap_data, features, max_display=20, plot_type="bar")
+    shap.summary_plot(shap_data, features, max_display=20, plot_type="dot")
+
